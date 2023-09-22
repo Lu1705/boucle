7b9e02f35dbc42e4c1e686125f00e0564eecfeba
Lu1705 <145431365+Lu1705@users.noreply.github.com>
2023-09-22 12:09:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let e=7; // nous créons une variable qui est égale à un nombre entier
+
+for(let i=0;i<e;i++){ // création d'une boucle : la valeur de départ de i est 0 ; i est inférieur à e (pour pas que la boucle soit infinie); et i augmente de 1 à chaque fois
+    console.log("nous sommes arrivé à 7");
+}
